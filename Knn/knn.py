bfec35f9c882a026dcf9c7423a9c29a85296bf19
Mandy Fan <mengdif@uw.edu>
2017-04-16 18:53:00 -0700

    format

--- Knn/knn.py
+++ Knn/knn.py
@@ -2,12 +2,10 @@ import pandas as pd
 def datainput(datacsv,testcsv):
     data_fram = pd.read_csv(datacsv)
     test_fram = pd.read_csv(testcsv)
-    print(test_fram)
     return data_fram,test_fram
 def distance(data_fram,test_fram):
     for a in range(len(test_fram.index)):
         data_fram[a] = ((data_fram.rWC-test_fram.rWC[a])**2+(data_fram.rCh-test_fram.rCh[a])**2)**0.5
-        print(data_fram)
     return data_fram
 def accuracy (test_fram,data_fram,k):
     ac = 0
@@ -17,9 +15,9 @@ def accuracy (test_fram,data_fram,k):
     for a in range(len(test_fram.index)):        
         for ik in reversed(range(1, k+1)):
             data_fram_sort = data_fram.sort_values([a], ascending=True).head(n=ik)
-            test_fram.loc[a,'pre'] = pd.value_counts(data_fram_sort['Type']).idxmax()
             if (len(pd.value_counts(data_fram_sort['Type']))) == 1 or (pd.value_counts(data_fram_sort['Type'])[0] > pd.value_counts(data_fram_sort['Type'])[1]):
-                break    
+                test_fram.loc[a,'pre'] = pd.value_counts(data_fram_sort['Type']).idxmax()   
+                break
         if test_fram.loc[a,'pre'] == test_fram.loc[a,'Type']:
             ac += 1
             acpercentage = ac/len(test_fram.index)
